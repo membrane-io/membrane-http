578622fdca1360783d7851faf04957dea638d9d9
Juan Campa <juancampa@gmail.com>
2022-12-23 10:10:55 -0500

    Add program status

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
-import { nodes } from 'membrane';
-const QUERY = '{ status headers body }';
+import { nodes } from "membrane";
+const QUERY = "{ status headers body }";
 
 export const Root = {
   get: ({ args }) => nodes.sys_http.get({ ...args }).$query(QUERY),
   post: ({ args }) => nodes.sys_http.post({ ...args }).$invoke(),
   put: ({ args }) => nodes.sys_http.put({ ...args }).$invoke(),
   patch: ({ args }) => nodes.sys_http.patch({ ...args }).$invoke(),
-  'delete': ({ args }) => nodes.sys_http.delete({ ...args }).$invoke(),
-}
+  delete: ({ args }) => nodes.sys_http.delete({ ...args }).$invoke(),
+  status: () => "Ready",
+};
